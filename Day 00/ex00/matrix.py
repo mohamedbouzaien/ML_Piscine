ee33ab5672d64d33c1d1edbda8e40e6ea87cde0e
Mohamed Bouzaien <mbouzaie@student.42.fr>
2023-04-24 23:56:45 +0200

    advancemect d00ex00

--- Day 00/ex00/matrix.py
+++ Day 00/ex00/matrix.py
@@ -6,31 +6,53 @@ class Matrix():
         self.shape = len(arg[0]), len(arg[0][0])
 
     def __add__(self, other):
+        if not isinstance(other, Matrix):
+            raise AttributeError("add: left operator isn't a Matrix")
+        if self.shape != other.shape:
+            raise ValueError("add: different shapes!")
         result = [[self.data[i][j] + other.data[i][j]
                   for j in range(self.shape[1])]
                   for i in range(self.shape[0])]
         return result
 
     def __radd__(self, other):
+        if not isinstance(other, Matrix):
+            raise AttributeError("add: left operator isn't a Matrix")
+        if self.shape != other.shape:
+            raise ValueError("add: different shapes!")
         result = [[other.data[i][j] + self.data[i][j]
                   for j in range(self.shape[1])]
                   for i in range(self.shape[0])]
         return result
 
     def __sub__(self, other):
+        if not isinstance(other, Matrix):
+            raise AttributeError("sub: left operator isn't a Matrix")
+        if self.shape != other.shape:
+            raise ValueError("add: different shapes!")
         result = [[self.data[i][j] - other.data[i][j]
                   for j in range(self.shape[1])]
                   for i in range(self.shape[0])]
         return result
 
     def __rsub__(self, other):
+        if not isinstance(other, Matrix):
+            raise AttributeError("sub: left operator isn't a Matrix")
+        if self.shape != other.shape:
+            raise ValueError("add: different shapes!")
         result = [[other.data[i][j] - self.data[i][j]
                   for j in range(self.shape[1])]
                   for i in range(self.shape[0])]
         return result
 
     def __mul__(self, other):
-        pass
+
+        result = Matrix(col=other.shape[1], row=self.shape[0])
+        for i in range(self.shape()[0]):
+            for j in range(other.shape()[1]):
+                for k in range(self.shape()[1]):
+                    result.data[i][j] += self.data[i][k] * other.data[k][j]
+        return result
 
     def __rmul__(self, other):
         pass
@@ -39,9 +61,8 @@ class Matrix():
         return self.__repr__()
 
     def __repr__(self):
-        line = "Matrix(["
-        line = [line + str(self.data[i]) for i in range(self.shape[0])]
-        return str(line)
+        line = [self.data[i] for i in range(self.shape[0])]
+        return "Matrix(" + str(line) + ")"
 
 
 class Vector(Matrix):
